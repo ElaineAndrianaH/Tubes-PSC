1ca37b398aa779dd26d9293f70dc544aa7e0db62
ElaineAndrianaHosni <6182001045@student.unpar.ac.id>
2022-12-04 21:40:06 +0700

    minesweeper

---
+++ MSCell.java
@@ -0,0 +1,79 @@
+public class MSCell {
+    ///TODO: Refactor this class so it doesn't have to know about the rest of
+    //the grid.
+	private boolean mine;
+	private boolean revealed;
+	private boolean flagged;
+	private boolean badFlag;
+	private int adjMines;
+
+    /**
+     * Construct an unmined, unrevealed cell with no adjacent mines.
+     */
+	public MSCell() {
+		mine = false;
+		revealed = false;
+		flagged = false;
+		badFlag = false;
+		adjMines = 0;
+	}
+
+    /**
+     * Copy an MSCell, but make it unrevealed and unflagged.
+     * @param m The MSCell to copy.
+     */
+	public MSCell(MSCell m) {
+		mine = m.isMine();
+		revealed = false;
+		flagged = false;
+		badFlag = false;
+		adjMines = m.getAdjMines();
+	}
+
+    /**
+     * Construct a cell.
+     * @param m Whether this cell is a mine.
+     */
+	public MSCell(boolean m) {
+		mine = m;
+		revealed = false;
+		flagged = false;
+		badFlag = false;
+	}
+
+	public boolean isMine() {
+		return mine;
+	}
+
+	public boolean isRevealed() {
+		return revealed;
+	}
+
+	public void setRevealed(boolean revealed) {
+		this.revealed = revealed;
+	}
+
+	public boolean isFlagged() {
+		return flagged;
+	}
+
+	public void setFlagged(boolean flagged) {
+		this.flagged = flagged;
+	}
+
+	public int getAdjMines() {
+		return adjMines;
+	}
+
+	public void setAdjMines(int adjacent) {
+		this.adjMines = adjacent;
+	}
+
+	public boolean isBadFlag() {
+		return badFlag;
+	}
+
+	public void setBadFlag(boolean badFlag) {
+		this.badFlag = badFlag;
+	}
+}
---
+++ Minesweeper.java
@@ -0,0 +1,54 @@
+import java.util.*; 
+public class Minesweeper {
+
+	static Scanner in;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Minesweeper! Type help for help.");
+
+		MSGrid ms = new MSGrid(16, 16, 40);
+		in = new Scanner(System.in);
+		long startTime = System.currentTimeMillis();
+
+		for (;;) {
+			System.out.println(ms);
+
+			if (ms.isGameOver()) {
+				System.out.println("Game over.");
+				break;
+			} else if (ms.checkWin()) {
+				System.out.println("You won!");
+				break;
+			}
+
+			System.out.print("?: ");
+			String command = in.next();
+
+			try {
+				if (command.equals("reveal")) {
+					ms.reveal(in.nextInt(), in.nextInt());
+				} else if (command.equals("quit")) {
+					break;
+				} else if (command.equals("revealAll")) {
+					ms.revealAll();
+				} else if (command.equals("flag")) {
+					ms.flag(in.nextInt(), in.nextInt());
+				} else if (command.equals("unflag")) {
+					ms.unflag(in.nextInt(), in.nextInt());
+				} else if (command.equals("help")) {
+					System.out.println("Commands: reveal, quit, revealAll, "
+							+ "flag, unflag.\nNormal usage: command row col.");
+				} else {
+					System.out.println("Didn't find command.");
+					in.nextLine();
+				}
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Bad coordinates. Try again.");
+			} catch (InputMismatchException e) {
+				System.out.println("Couldn't parse your input");
+			}
+		}
+
+		System.out.println("Bye!");
+	}
+}
